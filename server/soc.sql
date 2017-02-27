426395df33c6058d93697a0c58c612065b17629e
Padraig <pwredmond@gmail.com>
2017-02-27 18:55:56 +0000

    Updated database

--- server/soc.sql
+++ server/soc.sql
@@ -1,17 +1,16 @@
 -- Database for societies app
+-- Unique IDs 6 digits
+-- Names and emails 50 characters
 
-CREATE TABLE society(
-	id INTEGER(4) PRIMARY KEY,
-	pass_hash VARCHAR(64),
-	salt VARCHAR(64),
-	name VARCHAR(50),
-	email VARCHAR(50),
-	description VARCHAR(400),
-);
+DROP TABLE member_society;
+DROP TABLE committee_society;
+DROP TABLE admin;
+DROP TABLE society;
+DROP TABLE member;
 
 CREATE TABLE member(
-	member_id INTEGER(6) PRIMARY KEY
-	student_num VARCHAR(9) 
+	member_id INTEGER(6) PRIMARY KEY,
+	student_num VARCHAR(9),
 	pass_hash VARCHAR(64),
 	salt VARCHAR(64),
 	name VARCHAR(50),
@@ -20,12 +19,41 @@ CREATE TABLE member(
 	mobile VARCHAR(20),
 	emergency_ph VARCHAR(20),
 	date_joined DATE,
-	fullPartTime CHAR ENUM('F', 'P'),
-	verified CHAR ENUM('Y', 'N')
+	fullPartTime CHAR CHECK (fullPartTime IN ('F', 'P')),
+	verified CHAR CHECK (verfied IN ('Y', 'N'))
+);
+
+CREATE TABLE society(
+	society_id INTEGER(6) PRIMARY KEY,
+	pass_hash VARCHAR(64),
+	salt VARCHAR(64),
+	name VARCHAR(50),
+	email VARCHAR(50),
+	description VARCHAR(400),
+	chair_id INTEGER(6),
+
+	FOREIGN KEY(chair_id) REFERENCES member(member_id)
 );
 
 CREATE TABLE admin(
 	member_id INTEGER(6),
-	
+
 	FOREIGN KEY(member_id) REFERENCES member(member_id)
 );
+
+CREATE TABLE committee_society(
+	member_id INTEGER(6),
+	society_id INTEGER(6),
+
+	FOREIGN KEY(member_id) REFERENCES member(member_id),
+	FOREIGN KEY(society_id) REFERENCES society(society_id)
+);	
+
+CREATE TABLE member_society(
+	member_id INTEGER(6),
+	society_id INTEGER(6),
+
+	FOREIGN KEY(member_id) REFERENCES member(member_id),
+	FOREIGN KEY(society_id) REFERENCES society(society_id)
+);
+
