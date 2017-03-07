ebd8acea878a9f25b060403a1297ba0544b5e735
Padraig Redmond <pwredmond@gmail.com>
2017-03-07 16:54:37 +0000

    Added script to get socs for which they are committee

---
+++ server/api/get_user_committee.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import database
+import http
+
+member_id = http.post["member_id"].value
+session_id = http.post["session_id"].value
+
+if database.check_session(member_id, session_id):
+    sql = "SELECT society_id FROM committee_society WHERE(member_id = " + member_id + ");"
+    database.cur.execute(sql)
+    result = database.cur.fetchall()
+    list_of_socs = []
+    for row in result:
+        list_of_socs.append(str(row[0]))
+    response = http.generate_returncode(0)
+    response["society_id"] = list_of_socs
+else:
+    response = http.generate_returncode(1)
+
+# Send response
+http.send_response(response)
+
+# Close db connection
+database.close()
+
