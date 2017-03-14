d8722e482d1ff7da3247b726753f7eaf4e31a0d2
Simon Unsworth <SimonUnsworth@outlook.ie>
2017-03-14 20:31:59 +0000

    Added transition to main acitivity upon successful login

--- app/SocietiesApp/app/src/main/java/ie/dit/societiesapp/LoginActivity.java
+++ app/SocietiesApp/app/src/main/java/ie/dit/societiesapp/LoginActivity.java
@@ -3,6 +3,7 @@ package ie.dit.societiesapp;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -334,6 +335,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
             if (success) {
                 finish();
+
+                // if login is successful transition to main activity
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
