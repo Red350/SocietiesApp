00814187ac5886111cb38020fc9fcc7d75e58382
Simon Unsworth <SimonUnsworth@outlook.ie>
2017-03-15 10:31:31 +0000

    Added class to handle http post requests

---
+++ app/SocietiesApp/app/src/main/java/ie/dit/societiesapp/PostRequest.java
@@ -0,0 +1,13 @@
+package ie.dit.societiesapp;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by Simon on 15/03/2017.
+ */
+
+public abstract class PostRequest extends AsyncTask {
+    protected void doInBackGround(){
+
+    }
+}
