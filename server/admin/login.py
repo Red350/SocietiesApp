03871b99027820da6fa9c508fa6a26c0871fce41
Eoghan Quirke <eoghanquirke@gmail.com>
2017-03-24 15:42:56 +0000

    Added login Page

---
+++ server/admin/login.py
@@ -0,0 +1,61 @@
+#!C:\Python\python.exe
+
+import http
+import database
+import os
+import encrypt
+import uuid
+
+err = ""
+user = ""
+passwd = ""
+redirect = ""
+
+if os.environ['REQUEST_METHOD'] == 'POST':
+    if 'user' not in http.post:
+        err += 'Username required'
+    else:
+        user = http.post['user'].value
+    if 'pass' not in http.post:
+        err += ' Password required'
+    else:
+        passwd = http.post['pass'].value
+
+    if 'user' in http.post and 'pass' in http.post:
+        sql = "SELECT pass_hash, salt, admin_id FROM admin WHERE email like '%s'" % user
+        database.cur.execute(sql)
+
+        if database.cur.rowcount > 0:
+            row = database.cur.fetchone()
+            if encrypt.generate_hash(passwd, row[1]) == row[0]:
+                # assign a value
+                sesh = str(uuid.uuid4().hex)
+                sql = "INSERT INTO admin_session(admin_id, session_id)VALUES(%d,'%s')" % (row[2], sesh)
+                database.cur.execute(sql)
+                if(database.cur.rowcount > 0):
+                    print("Set-Cookie: session_id=%s; Max-Age=%d" % (sesh, 1 * 1 * 60 * 60))
+                    redirect = """<meta http-equiv="refresh" content="0; url=/admin/home.py">"""
+                else:
+                    err += "Database error"
+        else:
+            err += "Username or Password incorrect"
+
+database.close()
+
+output = """<html>
+            <head>
+            %s
+            <title>Login</title>
+            </head>
+            <body>
+                <form class="reg"action="login.py" method="post">
+                    <label for="user">User Name</label><input type="text" name="user" value="%s"> <br>
+                    <label for="pass">Password</label><input type="password" name="pass"> <br>
+                    <label for="Submit">Submit</label><input type="submit" value="Submit"> <br>
+                </form>
+                <p>%s</p>
+            </body>
+            </html>""" % (redirect, user, err)
+
+print("Content-Type: text/html\n")
+print(output)
