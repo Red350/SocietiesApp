f582f27df39b703b423d344965859264f3d8a0a4
Padraig Redmond <pwredmond@gmail.com>
2017-04-06 13:21:09 +0100

    Updated rest of the scripts to use new database and api classes

--- server/api/login.py
+++ server/api/login.py
@@ -2,24 +2,24 @@
 
 import uuid
 import database
-import http
+import api
 import encrypt
 
-# Send header
-http.send_json_header()
+db = database.Database()
+api = api.Api("json")
 
 # Ensure the correct post keys were sent
-if http.check_keys(("email", "password")):
-    email = http.post["email"].value
-    password = http.post["password"].value
+if api.check_keys(("email", "password")):
+    email = api.request["email"].value
+    password = api.request["password"].value
     
     # Check user's password, and that they are verified
     sql = "SELECT verified, pass_hash, salt, member_id FROM member WHERE( email = '" + email + "');"
-    database.cur.execute(sql)
-    if database.cur.rowcount == 1:
-        row = database.cur.fetchone()
+    db.cur.execute(sql)
+    if db.cur.rowcount == 1:
+        row = db.cur.fetchone()
         if row[0] == 'N':
-            response = http.generate_returncode(4)
+            response = api.set_returncode(4)
         else:
             # Check the users password
             pass_hash = row[1]
@@ -30,27 +30,27 @@ if http.check_keys(("email", "password")):
                 # Clear any old session IDs before generating a new one
                 member_id = row[3]
                 sql = "DELETE FROM session WHERE(member_id = " + str(member_id) + ");"
-                database.cur.execute(sql)
+                db.cur.execute(sql)
     
                 # Generate random session ID for user
                 session_id = str(uuid.uuid4().hex)
     
                 # Store session ID for that user in the database
                 sql = "INSERT INTO session VALUES(" + str(member_id) + ",'" + str(session_id) + "');"
-                database.cur.execute(sql)
+                db.cur.execute(sql)
     
-                response = http.generate_returncode(0)
-                response["session_id"] = session_id
-                response["member_id"] = member_id
+                response = api.set_returncode(0)
+                api.update_response("session_id", session_id)
+                api.update_response("member_id", member_id)
             else:
-                response = http.generate_returncode(2)
+                response = api.set_returncode(2)
     else:
-        response = http.generate_returncode(2)
+        response = api.set_returncode(2)
 else:
-    response = http.generate_returncode(5)
+    response = api.set_returncode(5)
 
 # Send response
-http.send_response(response)
+api.send_response()
 
 # Close db connection
-database.close()
+db.close()
