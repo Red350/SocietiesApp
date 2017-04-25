8eb42c478ae572b9f2c33285c944ad29e322334d
equirke <eoghanquirke@gmail.com>
2017-04-25 20:03:39 +0100

    Changed committee api to take emails
    Committee members are now added using
    their unique email addresses

--- server/api/add_committee.py
+++ server/api/add_committee.py
@@ -7,22 +7,33 @@ db = database.Database()
 api = api.Api("json")
 
 # Ensure the correct post keys were sent
-if api.check_keys(("member_id", "session_id", "society_id", "committee_id")):
+if api.check_keys(("member_id", "session_id", "society_id", "committee_email")):
     chair_id = api.request["member_id"].value
     session_id = api.request["session_id"].value
     society_id = api.request["society_id"].value
-    member_id = api.request["committee_id"].value
+    member_email = api.request["committee_email"].value
     
     if db.check_session(chair_id, session_id):
         if db.check_chair(chair_id, society_id):
-            # Add member as committee member
-            sql = "INSERT INTO committee_society(member_id, society_id) VALUES(" + member_id + ", " + society_id + ");"
+            sql = "SELECT member_id FROM member WHERE email LIKE '" + member_email +"'"
+            
             try:
                 db.cur.execute(sql)
                 api.set_returncode(0)
             except:
-                # Database error
                 api.set_returncode(6)
+                
+            if db.cur.rowcount == 1:
+                row = db.cur.fetchone()
+                member_id = str(row[0])
+                # Add member as committee member
+                sql = "INSERT INTO committee_society(member_id, society_id) VALUES(" + member_id + ", " + society_id + ");"
+                try:
+                    db.cur.execute(sql)
+                    api.set_returncode(0)
+                except:
+                    # Database error
+                    api.set_returncode(6)
         else:
             # Invalid permissions
             api.set_returncode(7)
